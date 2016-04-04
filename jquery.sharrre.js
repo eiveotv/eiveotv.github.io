35319774f5345e0319f77a4f84d7266d7d9165e6
La verdad oculta <laverdadoculta2@gmail.com>
2016-04-04 13:20:09 -0300

    asdfasdasd
    fadsfasdasdf

--- jquery.sharrre.js
+++ jquery.sharrre.js
@@ -35,6 +35,7 @@
     hide: function(){}, //personalize hide event with this callback function
     click: function(){}, //personalize click event with this callback function
     render: function(){}, //personalize render event with this callback function
+    buttons: {  //settings for buttons
       googlePlus : {  //http://www.google.com/webmasters/+1/button/
         url: '',  //if you need to personnalize button url
         urlCount: false,  //if you want to use personnalize button url on global counter
@@ -130,26 +131,7 @@
       //if somenone find a solution, mail me !
     }
   },
-  /* Popup for each social network
-  ================================================== */
-  popup = {
-    googlePlus: function(opt){
-      window.open("https://plus.google.com/share?hl="+opt.buttons.googlePlus.lang+"&url="+encodeURIComponent((opt.buttons.googlePlus.url !== '' ? opt.buttons.googlePlus.url : opt.url)), "", "toolbar=0, status=0, width=900, height=500");
-    },
-    facebook: function(opt){
-      window.open("http://www.facebook.com/sharer/sharer.php?u="+encodeURIComponent((opt.buttons.facebook.url !== '' ? opt.buttons.facebook.url : opt.url))+"&t="+opt.text+"", "", "toolbar=0, status=0, width=900, height=500");
-    },
-    twitter: function(opt){
-      window.open("https://twitter.com/intent/tweet?text="+encodeURIComponent(opt.text)+"&url="+encodeURIComponent((opt.buttons.twitter.url !== '' ? opt.buttons.twitter.url : opt.url))+(opt.buttons.twitter.via !== '' ? '&via='+opt.buttons.twitter.via : ''), "", "toolbar=0, status=0, width=650, height=360");
-    },
-    linkedin: function(opt){
-      window.open('https://www.linkedin.com/cws/share?url='+encodeURIComponent((opt.buttons.delicious.url !== '' ? opt.buttons.delicious.url : opt.url))+'&token=&isFramed=true', 'linkedin', 'toolbar=no,width=550,height=550');
-    },
-    pinterest: function(opt){
-      window.open('http://pinterest.com/pin/create/button/?url='+encodeURIComponent((opt.buttons.pinterest.url !== '' ? opt.buttons.pinterest.url : opt.url))+'&media='+encodeURIComponent(opt.buttons.pinterest.media)+'&description='+opt.buttons.pinterest.description, 'pinterest', 'toolbar=no,width=700,height=300');
-    }
-  };
-
+  
   /* Plugin constructor
   ================================================== */
   function Plugin( element, options ) {
